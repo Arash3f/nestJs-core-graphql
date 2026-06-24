7d987cd3d1c4e5be3e248232912ab847c427d4e5
arash alfooneh <arash.alfooneh@gmail.com>
2026-06-24 18:22:59 +0330

    :hammer: ref(global): prisma migrations

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-    extends: ["gitmoji"],
-    rules: {
-        "header-max-length": [2, "always", 100],
-    },
+  extends: ["gitmoji"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", "fix", "docs", "ref", "perf", "pack",
+        "wip", "Bcode", "version", "deploy",
+        "build", "ci", "refactor", "revert", "style", "test", "chore",
+      ],
+    ],
+  },
 }
